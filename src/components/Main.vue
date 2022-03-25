e06e53a2731db46c852a63e24828400a9298d3db
Od1e <stopsniffingaxe@gmail.com>
2022-03-25 19:08:01 -0400

    Add app basics

---
+++ src/components/Main.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="main">
+      <div id="topBar">
+          <h1>Artist Search</h1>
+          <SearchBar/>
+      </div>
+
+      <div id="moduleContent">
+          
+      </div>
+  </div>
+</template>
+
+<style scoped>
+    * {
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    }
+
+    #topBar {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        flex-basis: 100%;
+        flex-wrap: wrap;
+    }
+</style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import SearchBar from "./SearchBar.vue";
+import ArtistAlbums from "./ArtistAlbums.vue";
+import SimilarArtists from "./SimilarArtists.vue";
+import LastFMDisplay from "./LastFMDisplay.vue";
+
+@Component({
+  components: {
+    SearchBar, ArtistAlbums, SimilarArtists, LastFMDisplay,
+  },
+})
+export default class App extends Vue {}
+</script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="SearchBar">
+        <input type="text" placeholder="Enter an artist to begin" v-model.lazy="artist">
+    </div>
+</template>
+
+<style scoped>
+    input {
+        padding: 10px 50px;
+        border-radius: 10px;
+        border: 2px solid gray;
+        font-size: 20px;
+        text-align: center;
+    }
+</style>
+
+<script lang="ts">
+    import {Vue, Component} from "vue-property-decorator";
+
+    @Component
+    export default class MainComponent extends Vue {
+        artist = "";
+    }
+</script>
