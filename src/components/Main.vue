1be67192bd806c9fdf62c32eabe1e0f2f74d379e
Od1e <stopsniffingaxe@gmail.com>
2022-03-26 12:47:14 -0400

    Add axiois requests

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="main">
-      <div id="topBar">
+      <div class="flex" id="topBar">
           <h1>Artist Search</h1>
           <SearchBar @artist-changed="getArtistInfo" />
           <p>Hit enter to search</p>
       </div>
 
       <div id="moduleContent">
+          <div class="flex" id="topAlbumsModule">
 
+          </div>
+
+          <div class="flex" id="similarArtistsModule">
+
+          </div>
+
+          <div id="LastFMDisplayModule">
+
+          </div>
       </div>
   </div>
 </template>
@@ -17,10 +27,13 @@
         font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     }
 
-    #topBar {
+    .flex {
         display: flex;
         align-items: center;
         justify-content: center;
+    }
+
+    #topBar {
         flex-direction: column;
         flex-basis: 100%;
         flex-wrap: wrap;
@@ -30,11 +43,23 @@
         color: gray;
         font-size: 12px;
     }
+
+    #moduleContent {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: row;
+        flex-basis: 100%;
+    }
+
+    #topAlbumsModule, #similarArtistsModule {
+        flex-direction: column;
+    }
 </style>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import axios, { AxiosResponse } from "axios";
+import axios, { Axios, AxiosResponse } from "axios";
 import { similarArtistsResponse ,topAlbumsResponse, TopAlbums, SimilarArtists, albums, artists, images } from "../datatypes";
 
 import SearchBar from "./SearchBar.vue";
@@ -51,6 +76,10 @@ import LastFMDisplay from "./LastFMDisplay.vue";
 export default class MainComponent extends Vue {
     apiKey = "c5acce9c66b154433c1e5358fff84170";
     fetchAmountAlbums = 5;
+    fetchAmountSimArtists = 5;
+
+    topAlbums!: TopAlbums;
+    similarArtists!: SimilarArtists;
 
     getArtistInfo(artist: string): void {
         console.log(`Searching artist: ${artist}`);
@@ -63,7 +92,21 @@ export default class MainComponent extends Vue {
             autocorrect: 1, api_key: this.apiKey, format: "json" },
             })
             .then((r: AxiosResponse) => r.data)
-        
+            .then((returnedAlbums: topAlbumsResponse) => {
+                this.topAlbums = returnedAlbums.topalbums;
+            })
+
+        axios
+            .request({
+               method: "GET",
+            url: "https://ws.audioscrobbler.com/2.0",
+            params: { method: "artist.getSimilar", artist: artist, limit: this.fetchAmountSimArtists,
+            autocorrect: 1, api_key: this.apiKey, format: "json" } 
+            })
+            .then((r: AxiosResponse) => r.data)
+            .then((returnedArtists: similarArtistsResponse) => {
+                this.similarArtists = returnedArtists.similarartists;
+            })
     }
 }
 </script>
